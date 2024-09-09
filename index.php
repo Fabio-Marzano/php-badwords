ab2946f98d8c6c4fda636b10576730fe4d00574b
fabiomarzano <fabiomarzano1993@gmail.com>
2024-09-09 16:41:19 +0200

    [UPD] create-structur-html

--- index.php
+++ index.php
@@ -7,6 +7,33 @@
     <title>Document</title>
 </head>   
 <body>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">PHP Badwords</h1>
+            </div>
+            <div class="col-12">
+                <form action="./badwords.php" method="GET">
+                    <div class="row gy-3">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="" class="control-label">Paragrafo</label>
+                                <textarea name="paragraph" id="paragraph" class="form-control" placeholder="Paragrafo"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="" class="control-label">Parola da censurare</label>
+                                <textarea name="paragraph" id="paragraph" class="form-control" placeholder="Parola da censurare"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-sm btn-primary">Invia</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
